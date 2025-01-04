9a727a5685b594d2f4a9a9c84e0fd6be51d3735f
Rohit Sahu <sahu86744@gmail.com>
2025-01-04 23:33:33 +0530

    initial commit

---
+++ out/_next/static/chunks/252f366e-4752d295f30f9626.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[916],{1588:(t,a,h)=>{h.d(a,{uJN:()=>e});var v=h(4223);function e(t){return(0,v.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"}}]})(t)}}}]);
---
+++ out/_next/static/chunks/pages/tablemanagement-e9fc57f425a4bae6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[597],{9333:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/tablemanagement",function(){return e(8533)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[916,69,407,148,151,431,533,636,593,792],()=>n(9333)),_N_E=_.O()}]);
